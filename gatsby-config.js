48ab4ff7fc46091b69b735687ed5154d0a960dbd
Joshua Sawyer <joshua@jcsawyer.me>
2021-02-12 13:15:44 +0000

    Add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    siteMedadata: {
+        title: "Computing Society",
+        decription: "",
+        author: "jcsawyer123",
+        twitter: "jcsawyer123",
+        github: "jcsawyer123",
+        siteUrl: "https://www.computingsociety.co.uk"
+    },
     plugins: [
 
     ]
