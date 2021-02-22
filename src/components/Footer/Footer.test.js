e2f965833e538030c0c0ac12d280bb37ed8335bd
Joshua Sawyer <joshua@jcsawyer.me>
2021-02-22 17:02:26 +0000

    Add Footer Component

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,25 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Footer from "./Footer"
+
+
+// Using SnapShot testing
+// This ensures that a change we makes elesewhere does not change the behaviour of existing components
+// If the test fails due to an INTENTIONAL change run `jest --updateSnapshot`
+describe("Header", () => {
+
+    // What we are testing
+    it("renders correct", () => {
+        const tree = renderer
+    
+        // Crate the component
+        .create(<Header />)
+        .toJSON() // Convert to JSON
+
+        // Check if it matches what we expect by checking a snapshot from the past
+        // https://jestjs.io/docs/en/snapshot-testing
+        expect(tree).toMatchSnapshot()
+    })
+
+})
