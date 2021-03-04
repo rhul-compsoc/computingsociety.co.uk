6bbe036a1e6078c1ffb2e9339760e81487705fc8
Joshua Sawyer <joshua@jcsawyer.me>
2021-03-04 18:28:18 +0000

    Remove undeeded information + typo corrections

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,72 +6,13 @@ import * as React from "react"
 // ==============
 import Layout from "../components/Layout"
 import HeroSection from "../components/HeroSection"
-import MembershipSection from "../components/MebershipSection"
+import MembershipSection from "../components/MembershipSection"
 import EventSection from "../components/EventSection"
 import PastEventSection from "../components/PastEventSection"
 import TeamSection from "../components/TeamSection"
 import SupportedBySection from "../components/SupportedBySection/SupportedBySection"
 import SponsorSection from "../components/SponsorSection/SponsorSection"
 
-// ==============
-// STYLES
-// ==============
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
 
 // ==============
 // DATA
@@ -126,10 +67,8 @@ const IndexPage = () => {
           tag="All of what we do is generously supported by our partners in industry.">
         </SupportedBySection>
 
-        <SponsorSection>
-          
-        </SponsorSection>
-
+        <SponsorSection / >
+        
     </Layout>
 
   )
