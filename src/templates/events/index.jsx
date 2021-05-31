a797e0d15884e98e8e04db1319aa6cb736537e13
Leondro Lio <github@leondrolio.com>
2021-05-31 01:45:24 +0100

    Add SEO to event pages

--- src/templates/events/index.jsx
+++ src/templates/events/index.jsx
@@ -1,24 +1,30 @@
-import { graphql } from "gatsby"
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import React from "react"
-import Layout from "../../components/Layout/Layout"
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import SiteSEO from "../../components/SiteSEO";
 
 const EventTemplate = ({ data }) => {
-  const { body } = data.mdx
+  const { body, frontmatter } = data.mdx;
 
   return (
     <Layout>
+      <SiteSEO title={frontmatter.name} description={frontmatter.full_description} />
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
-  )
-}
+  );
+};
 
-export default EventTemplate
+export default EventTemplate;
 
 export const pageQuery = graphql`
   query($slug: String!) {
     mdx(fields: { slug: { eq: $slug } }) {
       body
+      frontmatter {
+        name
+        full_description
+      }
     }
   }
-`
+`;
