20b85072c04828a3e2742e6cb81cc7d708958b20
Leondro Lio <github@leondrolio.com>
2021-07-30 07:07:01 +0100

    Let height of hero section be adjusted

--- src/components/FreshersPictureHeroSection/index.js
+++ src/components/FreshersPictureHeroSection/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import HeroSection from "../HeroSection/HeroSection";
 import { container, freshersHeroSection, subtitle, title } from './index.module.scss';
 
-const FreshersPictureHeroSection = () => (
+const FreshersPictureHeroSection = ({
+  height = "full",
+  children
+}) => (
   <HeroSection
     name="RHUL CompSoc"
-    height="full"
+    height={height}
     className={freshersHeroSection}
   >
     <div className={container}>
       <h1 className={title}>Fresher's</h1>
       <h2 className={subtitle}>Welcome Week 2021</h2>
     </div>
+    {children}
   </HeroSection>
 )
 
