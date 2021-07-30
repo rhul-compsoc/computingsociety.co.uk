dd1c1cb45c7ea6e92dd82669d041c9fb26d65155
Leondro Lio <github@leondrolio.com>
2021-07-30 17:40:37 +0100

    Add gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-netlify",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-mdx",
       options: {
