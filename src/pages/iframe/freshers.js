8fa57e17e588e1bb51fee7e9b0ec160d450922f4
Leondro Lio <github@leondrolio.com>
2021-08-04 17:32:38 +0100

    Replace iframe with freshers iframe

---
+++ src/pages/iframe/freshers.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FreshersPictureHeroSection } from "../../components/FreshersPictureHeroSection";
+import SiteButton from "../../components/SiteButton";
+
+// markup
+const FreshersPage = () => {
+  return (
+    <FreshersPictureHeroSection height="fullscreen">
+      <SiteButton className="btn-solid-red m-1" to="/events/freshers2021">
+        See upcoming events
+      </SiteButton>
+    </FreshersPictureHeroSection>
+  );
+};
+
+export default FreshersPage;
