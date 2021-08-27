3ac096a5798ad1392d462a3364bd46e88966d62e
Leondro Lio <github@leondrolio.com>
2021-08-27 13:10:47 +0100

    Include ALL previous committee members

--- src/components/TeamSection/TeamSection.jsx
+++ src/components/TeamSection/TeamSection.jsx
@@ -6,10 +6,15 @@ import Section from "../Section/Section";
 import { SiteLink } from "../SiteClickable";
 
 const TeamSection = ({ name, tag }) => {
-  const [expanded, setExpanded] = useState(false);
-
   return (
-    <Section className="team" height="full" theme="dark" id="team_section" name={name} tag={tag}>
+    <Section
+      className="team"
+      height="full"
+      theme="dark"
+      id="team_section"
+      name={name}
+      tag={tag}
+    >
       <div className="container py-3">
         <div className="row justify-content-center">
           <div className="col-12 col-md-10 col-xl-8">
@@ -55,43 +60,52 @@ const TeamSection = ({ name, tag }) => {
                         new Date(b.frontmatter.event_start) -
                         new Date(a.frontmatter.event_start)
                     )
-                    .filter((_, index) => expanded || index === 0)
-                    .map(({ node }) => (
-                      <div key={node.id} className="py-4">
-                        <h4>{node.frontmatter.shortname}</h4>
-                        <div className="row">
-                          {node.frontmatter.people.map((person) => (
-                            <div className="col-6 col-sm-4 col-md-3 py-3" key={`${node.id}-${person.id?.frontmatter.name}-${person.role}`}>
-                              <div className="card col-12">
-                                <div className="card-picture">
-                                  {person.id?.frontmatter.image && (
-                                    <GatsbyImage
-                                      fluid={{
-                                        ...person.id.frontmatter.image
-                                          .childImageSharp.fluid,
-                                        aspectRatio: 1,
-                                      }}
-                                      className="picture"
-                                    />
-                                  )}
-                                </div>
-                                <div className="card-content">
-                                  <div className="text-center">
-                                    <span className="card-person name">
-                                      {person.id?.frontmatter.name}
-                                    </span>
-                                    <br />
-                                    <span className="card-person role">
-                                      {person.role}
-                                    </span>
+                    .filter((_, index) => index === 0)
+                    .map(({ node }) => {
+                      const peopleWithPictures = node.frontmatter.people.filter(person => person.id.frontmatter.image)
+                      const peopleWithoutPictures = node.frontmatter.people.filter(person => !person.id.frontmatter.image)
+                      const id = `committee-${node.frontmatter.shortname.replaceAll(' ', '')}`
+
+                      return (
+                        <div key={node.id} className="py-4">
+                          <h4 id={id}>{node.frontmatter.shortname}</h4>
+                          <div className="row">
+                            {[...peopleWithPictures, ...peopleWithoutPictures].map((person) => (
+                              <div
+                                className="col-6 col-sm-4 col-md-3 py-3"
+                                key={`${node.id}-${person.id?.frontmatter.name}-${person.role}`}
+                              >
+                                <div className="card col-12">
+                                  <div className="card-picture">
+                                    {person.id?.frontmatter.image && (
+                                      <GatsbyImage
+                                        fluid={{
+                                          ...person.id.frontmatter.image
+                                            .childImageSharp.fluid,
+                                          aspectRatio: 1,
+                                        }}
+                                        className="picture"
+                                      />
+                                    )}
+                                  </div>
+                                  <div className="card-content">
+                                    <div className="text-center">
+                                      <span className="card-person name">
+                                        {person.id?.frontmatter.name}
+                                      </span>
+                                      <br />
+                                      <span className="card-person role">
+                                        {person.role}
+                                      </span>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                          ))}
+                            ))}
+                          </div>
                         </div>
-                      </div>
-                    ))
+                      );
+                    })
                 }
               </StaticQuery>
             </div>
@@ -99,15 +113,9 @@ const TeamSection = ({ name, tag }) => {
         </div>
       </div>
 
-      {expanded ? (
-        <SiteLink onClick={() => setExpanded(false)}>
-          See only current committee
-        </SiteLink>
-      ) : (
-        <SiteLink onClick={() => setExpanded(true)}>
-          See previous committee
-        </SiteLink>
-      )}
+      <SiteLink to="/committee">
+        See previous committees
+      </SiteLink>
     </Section>
   );
 };
