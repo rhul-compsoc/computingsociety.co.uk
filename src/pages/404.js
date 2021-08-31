d55e6d6bc66accb6ccfb3df8f65a0b52a8c7cb72
Leondro Lio <leondrolio@gmail.com>
2021-08-31 15:54:12 +0100

    Add a new 404 image with a broken down sign

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 import Section from "../components/Section/Section";
 import { SiteLink } from "../components/SiteClickable";
 import SiteSEO from "../components/SiteSEO";
+import image from "../images/20210831_153548_cropped.jpg"
 
 // markup
 const NotFoundPage = () => {
@@ -12,6 +13,8 @@ const NotFoundPage = () => {
       <Section>
         <h1>OwO What's This?</h1>
         <p>The page you're looking for doesn't exist, no longer exists or has never existed.</p>
+
+        <img src={image} alt="A deconstructed sign from Royal Holloway, scattered over the grass. Segments of the sign include the International Building, Bourne Building and the Bedford Building." />
 
         <SiteLink to="/">Try going home</SiteLink>
       </Section>
