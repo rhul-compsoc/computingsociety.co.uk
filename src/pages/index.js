0c6ccfa7a8f0c86b40c7f1ac62635a7da0b7bc94
Danny Piper <djpiper28@gmail.com>
2021-10-27 17:19:14 +0100

    no freshers
    i squashed them all

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,11 +53,13 @@ const IndexPage = () => {
         </div>
       </HeroSection>
 
-      <FreshersPictureHeroSection height="half">
+      {/* <FreshersPictureHeroSection
+        height="half"
+      >
         <SiteButton className="btn-solid-red m-1" to="/events/freshers2021">
           See Upcoming Events
         </SiteButton>
-      </FreshersPictureHeroSection>
+      </FreshersPictureHeroSection> */}
 
       <MembershipSection
         name="Membership"
