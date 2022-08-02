229fbc7e8651514b355431d42604c929cb8b3cb6
chazzox <caylott@outlook.com>
2022-08-02 17:57:24 +0100

    feat: added chariOS as dep and added tailwind

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className='min-h-screen bg-slate-300'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
