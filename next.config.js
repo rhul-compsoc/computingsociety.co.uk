e2d25456ceeef3ad330d64dc51d5302308bdda4b
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-02 14:32:07 +0100

    feat: configured for exporting as static site

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
+module.exports = nextConfig;
