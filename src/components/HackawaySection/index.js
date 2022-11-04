9bb49d3378698075a67c5594a88f4ec9b4127701
Steph <58223099+stephanie-flower@users.noreply.github.com>
2022-11-04 15:39:17 +0000

    updates
    - removed freshers banner
    - replaced with hackaway banner
    - added upcoming events

---
+++ src/components/HackawaySection/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import HeroSection from "../HeroSection/HeroSection";
+import {
+  container,
+  hackawayHeroSection,
+  subtitle,
+  title,
+} from "./index.module.scss";
+import HackawayLogo from "./hackaway-logo.png";
+
+const HackawayPictureHeroSection = ({ height = "full", children }) => (
+  <HeroSection
+    name=""
+    height={height}
+    className={hackawayHeroSection}
+  >
+    <div className={container}>
+      <img src={HackawayLogo} alt="Royal Hackaway V6" />
+      <h2 className={subtitle}>4th - 5th February 2023</h2>
+    </div>
+    {children}
+  </HeroSection>
+);
+
+export { HackawayPictureHeroSection };
