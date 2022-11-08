db860370cb99a1ee9a877db1b47c940a80d42f20
chazzox <caylott@outlook.com>
2022-11-08 16:12:12 +0000

    fix: added button to git

---
+++ src/components/super-button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let onClick: () => void;
+</script>
+
+<button on:click={onClick} class="grid grid-cols-3 h-11 p-1 w-11 gap-[6px]">
+    <!-- row 1 -->
+    <div class='dot'/>
+    <div class='dot'/>
+    <div class='dot'/>
+    
+    <!-- row 2 -->
+    <div class='dot'/>
+    <div class='dot'/>
+    <div class='dot'/>
+    
+    <!-- row 3 -->
+    <div class='dot'/>
+    <div class='dot'/>
+    <div class='dot'/>
+</button>
+
+<style>
+    .dot {
+        @apply bg-white rounded-full ;
+    }
+</style>
