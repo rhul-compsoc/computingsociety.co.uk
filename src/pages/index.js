812e70075773d46c6eb9ea82eb0a1479fdd4d42e
Leo Durrant <poweredbykurisu@hotmail.com>
2023-02-20 19:20:11 +0000

    fix: relative paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ const IndexPage = () => {
           </SiteButton>
           <SiteButton
             className="btn-solid-discord m-1"
-            href="https://computingsociety.co.uk/dc"
+            href="/dc"
           >
             Join Discord
           </SiteButton>
