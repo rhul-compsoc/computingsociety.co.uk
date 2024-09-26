674eca511f2de5922c18f4e0e90f9265fc101a7d
Zakariya <mzkhaled468@gmail.com>
2024-09-26 17:54:02 +0100

    Init lol

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
---
+++ src/App.svelte
@@ -0,0 +1,3 @@
+<main>
+    <h1>CompSoc lol</h1>
+</main>
