b9dd42e84da9b175419f0d053ffebb02793ebffa
Zakariya <mzkhaled468@gmail.com>
2024-09-27 15:02:46 +0100

    Header looks nice now!

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,18 +1,44 @@
-<script lang="ts">
+<script>
   import css from "./Header.module.css";
-  import logo from "../assets/compsoc-logo.png";
+  import { onMount } from "svelte";
+
+  let scrollTop = 0;
+
+  function handleScroll() {
+    scrollTop = window.scrollY;
+  }
+
+  // Attach the scroll event listener on component mount
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 </script>
 
-<header class={css.header} id="">
-  <div class={css.navbar}>
-    <p>Home</p>
-    <p>Events</p>
-    <p>About</p>
-    <p>Our Team</p>
-    <p>Sponsors</p>
+<header
+  class={`${css.header} ${scrollTop > 75 ? css.sticky : ""}`}
+  style="height: {scrollTop > 75 ? '10rem' : '75vh'}"
+>
+  <div class={css.content}>
+    <div class={css.topBar}>
+      <div class={css.logo}>
+        <img src="/compsoc-logo.png" alt="CompSoc Logo, back to top" />
+      </div>
+      <div class={css.links}>
+        <a>Home</a>
+        <a>Events</a>
+        <a>About</a>
+        <a>Our Team</a>
+        <a>Sponsors</a>
+      </div>
+      <img
+        src="/compsoc-logo.png"
+        alt="Logo"
+        class={scrollTop > 75 ? css.hidden : ""}
+      />
+    </div>
   </div>
 </header>
-
-<div class={css.topCard}>
-  <img src={logo} alt="CompSoc" />
-</div>
